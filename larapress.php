85b39392f4fd482ab59f35e406090929cfa7a5c9
Matteo Rosati <mrosati@h-art.com>
2015-09-02 11:09:37 +0200

    add init hook

--- larapress.php
+++ larapress.php
@@ -14,32 +14,37 @@ require_once 'vendor/illuminate/support/Illuminate/Support/helpers.php';
 
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
-$basePath = str_finish(get_template_directory(), '/');
-$controllersDirectory = $basePath . 'controllers';
+function init_larapress()
+{
+    $basePath = str_finish(get_template_directory(), '/');
+    $controllersDirectory = $basePath . 'controllers';
 
-Illuminate\Support\ClassLoader::register();
-Illuminate\Support\ClassLoader::addDirectories(array($controllersDirectory));
+    Illuminate\Support\ClassLoader::register();
+    Illuminate\Support\ClassLoader::addDirectories(array($controllersDirectory));
 
-$app = new Illuminate\Container\Container;
-Illuminate\Support\Facades\Facade::setFacadeApplication($app);
+    $app = new Illuminate\Container\Container;
+    Illuminate\Support\Facades\Facade::setFacadeApplication($app);
 
-$app['app'] = $app;
-$app['env'] = 'production';
+    $app['app'] = $app;
+    $app['env'] = 'production';
 
-with(new Illuminate\Events\EventServiceProvider($app))->register();
-with(new Illuminate\Routing\RoutingServiceProvider($app))->register();
+    with(new Illuminate\Events\EventServiceProvider($app))->register();
+    with(new Illuminate\Routing\RoutingServiceProvider($app))->register();
 
-if (file_exists($basePath . 'routes.php')) {
-    try {
-        require $basePath . 'routes.php';
+    if (file_exists($basePath . 'routes.php')) {
+        try {
+            require $basePath . 'routes.php';
 
-        $request = Illuminate\Http\Request::createFromGlobals();
-        $response = $app['router']->dispatch($request);
+            $request = Illuminate\Http\Request::createFromGlobals();
+            $response = $app['router']->dispatch($request);
 
-        $response->send();
+            $response->send();
 
-        exit(); // exit to skip other wordpress output
-    } catch (NotFoundHttpException $e) {
-        // just ignore 404 errors here
+            exit(); // exit to skip other wordpress output
+        } catch (NotFoundHttpException $e) {
+            // just ignore 404 errors here
+        }
     }
 }
+
+add_action('init', 'init_larapress');
